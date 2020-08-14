102f7cdbca83488569940ddb02233666e93a7289
eaarda <elifaarda@gmail.com>
2020-08-14 15:16:38 +0300

    flash messages added

--- Controllers/userControll.py
+++ Controllers/userControll.py
@@ -21,7 +21,7 @@ def login():
     email = request.form['email']
     password = request.form['password']
     if not email or not password:
-        print("eksik bilgi")
+        flash("Geçersiz email veya şifre","login")
     else:
         user = User.query.filter_by(email=email,password=password).first()
         login_user(user)
@@ -36,13 +36,11 @@ def signup():
     username = request.form['username']
     email = request.form['email']
     password = request.form['password']
-    if not username or not email or not password :
-        print("eksik bilgi")
-    
     existing_user = User.query.filter_by(email=email).first()
-    if existing_user:
-        print("already exists")
-        return render_template("index.html")
+    if not username or not email or not password :
+        flash("Tüm alanları doldurun!","signup")
+    elif existing_user:
+        flash("Geçersiz email adresi!","signup") 
     else:
         user = User(username=username, email=email, password=password)
         db.session.add(user)
